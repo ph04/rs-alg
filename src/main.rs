ba40909b952f8aa5ce5c6ead53d2adce8cf84926
ph04 <tacc010203@gmail.com>
2022-05-29 13:26:16 +0200

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use alg::heapsort::sort_by_heapsort;
 
 fn main() {
-    let mut array = [3, 6, 0, 4, 111, 10, 7, 2, 3, 4, 7, 3, 1, 0];
+    let mut array = [3, 6, 0, 4, 24, 10, 7, 2, 3, 4, 7, 3, 1, 0];
 
     sort_by_heapsort(&mut array);
 
---
+++ target/debug/deps/alg-fd47d8027daf3062.d
@@ -0,0 +1,5 @@
+c:\Users\Alessio\Desktop\codes\misc\rust\alg\rs-alg\target\debug\deps\alg-fd47d8027daf3062.rmeta: src\main.rs
+
+c:\Users\Alessio\Desktop\codes\misc\rust\alg\rs-alg\target\debug\deps\alg-fd47d8027daf3062.d: src\main.rs
+
+src\main.rs:
